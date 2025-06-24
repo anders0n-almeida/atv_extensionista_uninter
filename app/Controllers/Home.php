e6fe8c6565fac510ddb93d9cc25322419f7df9ed
Anderson de Almeida <andersondev999@gmail.com>
2025-06-24 19:17:52 -0300

    Finalização do modelo estático + Criação de alguns Models + Implementação inicial do Banco de Dados

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,11 +3,44 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\NoticiasModel;
+use App\Models\UsuariosModel;
 
 class Home extends BaseController
 {
     public function index() {
-        return view('index');
+
+        $noticias_model = new NoticiasModel();
+        $usuarios_model = new UsuariosModel();
+
+        $noticias_pag_inicial = $noticias_model->limit(4)->orderBy('id', 'DESC')->find();
+        $noticias_complementares = [];
+        $noticia_destaque = '';
+        
+        if (!empty($noticias_pag_inicial)) {
+
+            
+            for ($i = 0; $i < count($noticias_pag_inicial); $i++) {
+
+                if ($i == 0) {
+                    $noticia_destaque = $noticias_pag_inicial[$i];
+                    $noticia_destaque->publicador = $usuarios_model->find($noticia_destaque->publicado_por)->usuario;
+                }
+                else {
+                    $noticias_pag_inicial[$i]->publicador =  $usuarios_model->find($noticias_pag_inicial[$i]->publicado_por)->usuario;
+                    $noticias_complementares[] = $noticias_pag_inicial[$i];
+                }
+
+            }
+
+        }
+
+        $data = [
+            'noticias_pag_inicial' => $noticias_pag_inicial,
+            'noticia_destaque' => $noticia_destaque,
+            'noticias_complementares' => $noticias_complementares,
+        ];
+
+        return view('index', $data);
     }
 }
--- app/Views/index.php
+++ app/Views/index.php
@@ -53,6 +53,7 @@
         </div>
     </nav>
     
+    <!-- Bloco de introdução -->
     <div class="container-fluid">
         <div class="row align-items-center justify-content-center" id="intro-section">
             <div class="col-md-4 col-sm-12" id="intro-text">
@@ -65,22 +66,89 @@
         </div>
     </div>
 
+    <!-- Bloco de cursos/leituras -->
     <div class="container-fluid">
-        <div class="row align-item-center justify-content-center gap-3" id="courses-section">
-            <div class="course-box">
+        <div class="row justify-content-center gap-3" id="courses-section">
+            <a href="#" class="course-box">
                 <h4>Introdução à Organização Financeira</h4>
                 <p>Conceitos básicos sobre finanças pessoais para iniciar sua jornada</p>
-            </div>
-            <div class="course-box">
+            </a>
+            <a href="#" class="course-box">
                 <h4>Investimentos de Renda Fixa</h4>
                 <p>Definição e breve ensaio sobre opções do tipo mais seguro de investimento</p>
-            </div>
-            <div class="course-box">
+            </a>
+            <a href="#" class="course-box">
                 <h4>Orçamento e Economia</h4>
                 <p>Como trabalhar com orçamentos e dicas para economizar de forma inteligente</p>
+            </a>
+        </div>
+        <div class="row" id="all-courses-link">
+            <div class="col">
+                <a href="#">Ver todos >></a>
             </div>
         </div>
     </div>
+
+    <!-- Bloco de últimas notícias -->
+    <div class="container-fluid">
+        <div class="row justify-content-center" id="lastest-news">
+            <div class="col-md-8">
+                <h1 class="text-center mt-4" style="border-bottom: 1px solid var(--color-dark-blue); color: var(--color-dark-blue)">Últimas Notícias</h1>
+            </div>
+            <div class="row justify-content-center">
+                <?php if (!empty($noticias_pag_inicial)): ?>
+
+                    <div class="col-md-8 mb-4" id="featured-news">
+                        <h2><?= $noticia_destaque->titulo ?></h2>
+                        <p>
+                            <b>Data de Publicação</b>: <?= date('d/m/Y', strtotime($noticia_destaque->data_publicacao)) ?><br>
+                            <b>Publicado por</b>: <?= $noticia_destaque->publicador ?>
+                            <br>
+                            <a href="#" class="goto-read-link">Ler >></a>
+                        </p>
+                        <img src="<?= base_url('assets/news/' . $noticia_destaque->nome_arq_imagem) ?>" alt="Imagem da notícia <?= $noticia_destaque->id ?>" class="img-fluid">
+                    </div>
+
+                    <?php if (count($noticias_complementares) > 0): ?>
+                        <div class="row justify-content-center" id="adjacent-news">
+
+                            <?php foreach ($noticias_complementares as $noticia): ?>
+                                <div class="col-md-3 adjacent-news-box">
+                                    <img src="<?= base_url('assets/news/' . $noticia->nome_arq_imagem) ?>" alt="Imagem da notícia <?= $noticia->id ?>" class="img-fluid">
+                                    <h2 class="mt-2"><?= $noticia->titulo ?></h2>
+                                    <p>
+                                        Data de Publicação: <?= date('d/m/Y', strtotime($noticia->data_publicacao)) ?><br>
+                                        Publicado por: <?= $noticia->publicador ?>
+                                        <br>
+                                        <a href="#" class="goto-read-link">Ler >></a>
+                                    </p>
+                                </div>
+                            <?php endforeach ?>
+ 
+                        </div>
+                    <?php endif ?>
+                
+                <?php else: ?>
+
+                <?php endif ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- Rodapé/footer -->
+    <footer class="container-fluid">
+        <div class="row align-items-center justify-content-center" id="page-footer">
+            <div class="col-md-4 text-center">
+                Desenvolvido por Anderson d'Almeida &copy; 2025
+            </div>
+            <div class="col-md-4 text-center">
+                <h4>Redes Sociais</h4>
+                <a href="#" class="socials-label"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#" class="socials-label"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#" class="socials-label"><i class="fa-brands fa-x-twitter"></i></a>
+            </div>
+        </div>
+    </footer>
 
     <!-- Importação do módulo de JavaScript do Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
